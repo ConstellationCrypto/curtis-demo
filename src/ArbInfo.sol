36defcfebfb0e95b394ad474dbcd5e31954c6bc5
theappletucker <pyjou@stanford.edu>
2024-07-19 01:05:12 +0000

    simple contract that uses native yield

---
+++ src/ArbInfo.sol
@@ -0,0 +1,28 @@
+// Copyright 2021-2022, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro-contracts/blob/main/LICENSE
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity >=0.4.21 <0.9.0;
+
+/// @title Lookup for basic info about accounts and contracts.
+/// @notice Precompiled contract that exists in every Arbitrum chain at 0x0000000000000000000000000000000000000065.
+interface ArbInfo {
+    /// @notice Retrieves an account's balance
+    function getBalance(address account) external view returns (uint256);
+
+    /// @notice Retrieves a contract's deployed code
+    function getCode(address account) external view returns (bytes memory);
+
+    // fixed, shares, debt
+    function getBalanceValues(address account) external view returns (uint256, uint256, uint256);
+
+    // flags
+    function getYieldConfiguration(address account) external view returns (uint8);
+
+    // delegate
+    function getDelegate(address account) external view returns (address);
+
+    function configureAutomaticYield() external;
+    function configureVoidYield() external;
+    function configureDelegateYield(address account) external;
+}
--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ArbInfo} from "./ArbInfo.sol"; 
+
 contract Counter {
     uint256 public number;
+    ArbInfo arbInfo = ArbInfo(0x0000000000000000000000000000000000000065);
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -11,4 +14,18 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+    function setAutomaticYield() public {
+        arbInfo.configureAutomaticYield();
+    }
+
+    function setVoidYield() public {
+        arbInfo.configureVoidYield();
+    }
+
+    function setDelegateYield(address a) public {
+        arbInfo.configureDelegateYield(a);
+    }
+
+    receive() external payable  { }    
 }
